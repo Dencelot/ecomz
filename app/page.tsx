001c266e3f05f5b43df0d040bb342bbcb3227a22
Владислав Попов <vladislavpopov.dev@gmail.com>
2024-05-05 09:39:58 +0300

    policy

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Contacts } from './components/Contacts/Contacts'
 import Middle from "./components/Middle/Middle";
 import Top from "./components/Top/Top";
 
@@ -6,8 +5,7 @@ export default function Home() {
   return (
     <main className="flex-auto w-full minH ">
       <Top/>
-      {/* <Middle/> */}
-      <Contacts/>
+      <Middle/>
     </main>
   );
 }
