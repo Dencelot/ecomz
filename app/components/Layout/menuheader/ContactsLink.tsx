8d9aadcbbc40017c6d55227846664303dabe2513
Denis <dencelot.1@gmail.com>
2024-05-07 18:14:05 +0300

    header fix

--- app/components/Layout/menuheader/ContactsLink.tsx
+++ app/components/Layout/menuheader/ContactsLink.tsx
@@ -11,14 +11,14 @@ const ContactsLink = () => {
    }
   return (
    <li className='h-full'>
-      <Link href={'/contacts'} onClick={handleClick} className='flex items-center justify-center h-full md:px-[16px] md:text-[var(--bazalt-100)] md:hover:bg-[var(--sunrise-40)] text-[var(--white-100)] md:bg-[var(--white-100)] rounded-tl-[16px] rounded-bl-[16px] transition-colors'>
+      <button onClick={handleClick} className='flex items-center justify-center h-full md:px-[16px] md:text-[var(--bazalt-100)] md:hover:bg-[var(--sunrise-40)] text-[var(--white-100)] md:bg-[var(--white-100)] rounded-tl-[16px] rounded-bl-[16px] transition-colors'>
          <span className='md:mr-[12px]'>
             {data.header.popup}
          </span>
          <span className='md:block hidden'>
          <Burger active={active}/>
          </span>
-      </Link>
+      </button>
    
    </li>
   )
