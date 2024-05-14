3770850e3f420c13540ef896068203a15f4b4197
Denis <dencelot.1@gmail.com>
2024-05-14 18:14:22 +0300

    map fix

--- app/components/YandexMap/YandexMap.tsx
+++ app/components/YandexMap/YandexMap.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.scss'
 export const YandexMap = () => {
   return (
     <div className={`${styles.map}`}>
-      <YMaps>
+      {/* <YMaps>
         <Map
           style={{ height: '713px', width: '100%' }}
           defaultState={{
@@ -23,7 +23,8 @@ export const YandexMap = () => {
         >
           <Placemark geometry={[59.88309, 30.37005]} />
         </Map>
-      </YMaps>
+      </YMaps> */}
+      <div className='h-full' style={{position:"relative",overflow:"hidden"}}><a href="https://yandex.ru/maps/org/kontinent/1189678926/?utm_medium=mapframe&utm_source=maps" style={{color:"#eee",fontSize:"12px",position:"absolute",top:"0px"}}>Континент</a><a href="https://yandex.ru/maps/2/saint-petersburg/category/shopping_mall/184108083/?utm_medium=mapframe&utm_source=maps" style={{color:"#eee",fontSize:"12px",position:"absolute",top:"14px"}}>Торговый центр в Санкт‑Петербурге</a><a href="https://yandex.ru/maps/2/saint-petersburg/category/entertainment_center/184106372/?utm_medium=mapframe&utm_source=maps" style={{color:"#eee",fontSize:"12px",position:"absolute",top:"28px"}}>Развлекательный центр в Санкт‑Петербурге</a><iframe className='h-full w-full' src="https://yandex.ru/map-widget/v1/org/kontinent/1189678926/?ll=30.369813%2C59.883336&utm_source=share&z=14" width="560" height="400" frameBorder="1" allowFullScreen={true} style={{position:"relative"}}></iframe></div>
     </div>
   )
 }
